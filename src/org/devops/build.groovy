35043e031c4b6405d2256f336f3c321202aef7bd
LSL <263587092@qq.com>
2020-03-18 17:56:51 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -8,7 +8,9 @@ def  Build(buildType,buildShell){
   def buildTools = ["mvn":"m2","ant":"ANT","gradle":"GRADLE","npm":"NPM"];
   //定义一个叫做buildTools的字典.
   //key为具体的打包命令，value为jenkins当中全局工具某个具体工具的名称,这里如：m2是maven的全局配置工具自定义名称.
-                
+  
+  println("This time we build tool is :"+"${buildType}");
+  
   buildHome = tool buildTools[buildType];
   //定义一个buildHome 通过字典的形式将方法的buildType字段传入到字典buildTools当中，并将值赋值给buildHome变量.
   
